057b7248199572baa908d75fe1156877238b973a
Jason Jensen <jason.jensen@pnnl.gov>
2025-03-07 00:13:01 +0000

    Resolve "Add Claude 3.7 for Model Garden"

--- custom/scripts/models_config.py
+++ custom/scripts/models_config.py
@@ -2,6 +2,15 @@ from custom.scripts.utils import require_env_var
 
 models = [
     {
+        "model_name": "claude-3-7-sonnet-20250219-v1-openwebui",
+        "litellm_params": {
+            "model": "us.anthropic.claude-3-7-sonnet-20250219-v1:0", # note the model name starts with "us." because this model requires using an inference profile
+            "aws_access_key_id": require_env_var("BEDROCK_AWS_ACCESS_KEY_ID"),
+            "aws_secret_access_key": require_env_var("BEDROCK_AWS_SECRET_ACCESS_KEY"),
+            "aws_region_name": "us-west-2"
+        }
+    },
+    {
         "model_name": "claude-3-5-sonnet-20241022-v2-openwebui",
         "litellm_params": {
             "model": "anthropic.claude-3-5-sonnet-20241022-v2:0",
